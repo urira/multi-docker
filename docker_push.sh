39e0e3f55e5837bdaafa620807480d0f755e1b7f
varvar <michaels@websplanet.com>
2019-12-26 23:09:31 +0200

    docker push using bash

---
+++ docker_push.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "$DOCKER_PWD" | docker login -u "$DOCKER_ID" --password-stdin
+
+docker push rxxxabintex/multi-client
+docker push rabintex/multi-worker
+docker push rabintex/multi-nginx
+docker push rabintex/multi-server
